c2fb336abebb885f3a01f00f8479a91f652a0929
dissmiate001 <gleb.voznenko@e-u.edu.ua>
2026-03-17 22:06:12 +0700

    Update task03.rs

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,28 @@
 // https://www.hackerrank.com/challenges/staircase/problem
+
+pub fn staircase(n: i32) -> Vec<String> {
+    let mut result = Vec::new();
+
+    for i in 1..=n {
+        let spaces = " ".repeat((n - i) as usize);
+        let hashes = "#".repeat(i as usize);
+
+        result.push(format!("{}{}", spaces, hashes));
+    }
+
+    result
+}
+
+#[test]
+fn test0() {
+    let real = staircase(4);
+
+    let expected = vec![
+        "   #".to_string(),
+        "  ##".to_string(),
+        " ###".to_string(),
+        "####".to_string(),
+    ];
+
+    assert_eq!(real, expected);
+}
