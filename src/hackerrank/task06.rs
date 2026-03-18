094b591cfa38c36049dd54ffa1758ec30c129a6d
dissmiate001 <gleb.voznenko@e-u.edu.ua>
2026-03-18 12:48:17 +0700

    Update task06.rs

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,1 +1,18 @@
 // https://www.hackerrank.com/challenges/kangaroo/problem
+
+pub fn kangaroo(x1: i32, v1: i32, x2: i32, v2: i32) -> bool {
+    if v1 <= v2 {
+        return false;
+    }
+
+    (x2 - x1) % (v1 - v2) == 0
+}
+
+#[test]
+fn test0() {
+    let real = kangaroo(0, 3, 4, 2);
+
+    let expected = true;
+
+    assert_eq!(real, expected);
+}
