1f875b6615f5a577841bdbf38d8998407dba69c2
dissmiate001 <gleb.voznenko@e-u.edu.ua>
2026-03-18 12:33:56 +0700

    Update task04.rs

--- src/hackerrank/task04.rs
+++ src/hackerrank/task04.rs
@@ -1,1 +1,32 @@
 // https://www.hackerrank.com/challenges/grading/problem
+
+pub fn grading_students(grades: &[i32]) -> Vec<i32> {
+    let mut result = Vec::new();
+
+    for &g in grades {
+        if g < 38 {
+            result.push(g);
+        } else {
+            let next = ((g / 5) + 1) * 5;
+
+            if next - g < 3 {
+                result.push(next);
+            } else {
+                result.push(g);
+            }
+        }
+    }
+
+    result
+}
+
+#[test]
+fn test0() {
+    let grades = vec![73, 67, 38, 33];
+
+    let real = grading_students(&grades);
+
+    let expected = vec![75, 67, 40, 33];
+
+    assert_eq!(real, expected);
+}
