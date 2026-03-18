2c246378597d892f7b263468bef9efae2289f8ca
dissmiate001 <gleb.voznenko@e-u.edu.ua>
2026-03-18 13:53:09 +0700

    Update task09.rs

--- src/hackerrank/task09.rs
+++ src/hackerrank/task09.rs
@@ -1,1 +1,30 @@
 // https://www.hackerrank.com/challenges/migratory-birds/problem
+
+pub fn migratory_birds(arr: &[i32]) -> i32 {
+    let mut count = vec![0; 6];
+
+    for &x in arr {
+        count[x as usize] += 1;
+    }
+
+    let mut best = 1;
+
+    for i in 2..6 {
+        if count[i] > count[best] {
+            best = i;
+        }
+    }
+
+    best as i32
+}
+
+#[test]
+fn test0() {
+    let arr = vec![1, 4, 4, 4, 5, 3];
+
+    let real = migratory_birds(&arr);
+
+    let expected = 4;
+
+    assert_eq!(real, expected);
+}
