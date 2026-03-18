3974c88ebe94b3a98aeb9685c590d8feb26776d3
dissmiate001 <gleb.voznenko@e-u.edu.ua>
2026-03-18 13:58:12 +0700

    Update task10.rs

--- src/hackerrank/task10.rs
+++ src/hackerrank/task10.rs
@@ -1,1 +1,28 @@
 // https://www.hackerrank.com/challenges/sock-merchant/problem
+
+pub fn sock_merchant(arr: &[i32]) -> i32 {
+    let mut count = vec![0; 101];
+
+    for &x in arr {
+        count[x as usize] += 1;
+    }
+
+    let mut pairs = 0;
+
+    for c in count {
+        pairs += c / 2;
+    }
+
+    pairs
+}
+
+#[test]
+fn test0() {
+    let arr = vec![10, 20, 20, 10, 10, 30, 50, 10, 20];
+
+    let real = sock_merchant(&arr);
+
+    let expected = 3;
+
+    assert_eq!(real, expected);
+}
